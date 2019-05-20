2109acde9a1d3075390e7352acaa5abbab9343fd
League Student <league@AdministorsiMac.attlocal.net>
2019-05-19 17:28:02 -0700

    eclipse

---
+++ src/extra/Duck.java
@@ -0,0 +1,27 @@
+package extra;
+
+public class Duck {
+	int numberOfFriends;
+	String favoriteFood;
+	
+	void quack()  {
+		
+		
+	}
+	
+	void waddle() {
+		
+		
+	}
+	
+	
+	Duck(String favoriteFood, int numberOfFriends) {
+      	this.favoriteFood = favoriteFood;
+      	this.numberOfFriends = numberOfFriends;
+      	
+	}	
+
+	public static void main(String[] args) {
+	
+	}
+}
---
+++ src/extra/Pink.java
@@ -0,0 +1,13 @@
+package extra;
+
+public class Pink {
+public static void main(String[] args) {
+	Duck daffy = new Duck("donuts",5);
+
+
+    
+
+
+daffy.quack();	
+}
+}
